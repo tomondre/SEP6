9174493b27af8b4c7d161e060bea23e5efea777f
tomondre <tomas.ondrejka2000@gmail.com>
2023-05-01 12:13:58 +0900

    POC java gradle app init

---
+++ backend/src/main/java/com/sep6/backend/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.sep6.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
