a8443d056842494948d714ea4da3dce8eadeb8e9
Georgiana <ion.georgiana@hotmail.com>
2023-05-12 14:57:52 +0300

    Set-up typescript

--- frontend/sep-frontend/src/App.tsx
+++ frontend/sep-frontend/src/App.tsx
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 function App() {
+  const logo = require('./logo.svg');
+
   return (
     <div className="App">
       <header className="App-header">
