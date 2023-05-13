6d5efeacab3cc65eb1d0b716618f47d41e399dcb
tomondre <tomas.ondrejka2000@gmail.com>
2023-05-13 20:12:16 +0900

    Adding getters and setters to models

--- backend/src/main/java/com/sep6/backend/models/Genre.java
+++ backend/src/main/java/com/sep6/backend/models/Genre.java
@@ -1,5 +1,6 @@
 package com.sep6.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
@@ -11,6 +12,39 @@ public class Genre {
     @Id
     private int id;
     private String name;
+    @JsonIgnore
     @ManyToMany(mappedBy = "genres")
     private List<Movie> movies;
+
+    public Genre() {
+    }
+
+    public Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
 }
