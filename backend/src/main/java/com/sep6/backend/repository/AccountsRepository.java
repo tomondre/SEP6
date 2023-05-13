419814b5a2edbfa40a80379098ed7a18281178bb
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-13 17:28:54 +0200

    SEP6-9: implement repository pattern

--- backend/src/main/java/com/sep6/backend/repository/AccountsRepository.java
+++ backend/src/main/java/com/sep6/backend/repository/AccountsRepository.java
@@ -1,4 +1,11 @@
 package com.sep6.backend.repository;
 
+import com.sep6.backend.models.Account;
+
+import java.util.Optional;
+
 public interface AccountsRepository {
+    Optional<Account> findByEmail(String email);
+
+    Account save(Account user);
 }
