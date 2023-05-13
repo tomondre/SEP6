ed018ea167cf20318ecf969d04c824c1466346bb
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-13 15:39:12 +0200

    SEP6-9: authentication and registration model DTOs

---
+++ backend/src/main/java/com/sep6/backend/models/auth/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.sep6.backend.models.auth;
+
+import com.sep6.backend.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    private String country;
+    private String profilePictureUrl;
+    private String dateOfBirth;
+    private String gender;
+    private Role role;
+}
