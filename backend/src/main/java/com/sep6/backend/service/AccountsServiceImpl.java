b1aeb32daaafb19ebe0b341fc475957b159461db
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-15 13:28:04 +0200

    SEP6-21: logic for editing and deleting users

--- backend/src/main/java/com/sep6/backend/service/AccountsServiceImpl.java
+++ backend/src/main/java/com/sep6/backend/service/AccountsServiceImpl.java
@@ -1,12 +1,30 @@
 package com.sep6.backend.service;
 
+import com.sep6.backend.models.Account;
 import com.sep6.backend.repository.AccountsRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class AccountsServiceImpl implements AccountsService {
+
+    private PasswordEncoder passwordEncoder;
     private AccountsRepository repository;
+
+    @Override
+    public Optional<Account> editAccount(int id, Account account)
+    {
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        return repository.editAccount(id, account);
+    }
+
+    @Override
+    public void deleteAccount(int id)
+    {
+        repository.deleteAccount(id);
+    }
 }
