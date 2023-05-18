12b3d6b57c87011b0c688d6229319e6c5480a273
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-18 09:03:09 +0200

    SEP6-28: throw appropriate errors with error codes

--- backend/src/main/java/com/sep6/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/sep6/backend/controller/AuthenticationController.java
@@ -7,13 +7,17 @@ import com.sep6.backend.service.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/auth")
@@ -26,20 +30,57 @@ public class AuthenticationController
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request)
     {
-        return ResponseEntity.ok(service.register(request));
+        try
+        {
+            return ResponseEntity.ok(service.register(request));
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        catch (Exception e)
+        {
+            //TODO log the error
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong, please try again later");
+        }
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request)
     {
-        return ResponseEntity.ok(service.authenticate(request));
+        try
+        {
+            return ResponseEntity.ok(service.authenticate(request));
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        catch (AuthenticationException e)
+        {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+        catch (Exception e)
+        {
+            //TODO log the error
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong, please try again later");
+        }
     }
 
     @PostMapping("/refresh-token")
     public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
-        service.refreshToken(request, response);
+        try
+        {
+            service.refreshToken(request, response);
+        }
+        catch (NoSuchElementException e)
+        {
+            response.sendError(HttpStatus.NOT_FOUND.value(), "The refresh token is not valid");
+        }
+        catch (Exception e)
+        {
+            response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Something went wrong, please try again later");
+        }
     }
-
-
 }
