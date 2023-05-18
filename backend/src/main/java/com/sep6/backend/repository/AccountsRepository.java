32e240aa073e7a8681284cc6358a81b55fe4410d
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-18 09:02:42 +0200

    SEP6-28: added exceptions to methods

--- backend/src/main/java/com/sep6/backend/repository/AccountsRepository.java
+++ backend/src/main/java/com/sep6/backend/repository/AccountsRepository.java
@@ -1,14 +1,20 @@
 package com.sep6.backend.repository;
 
+import com.sep6.backend.exceptions.EditException;
+import com.sep6.backend.exceptions.SaveException;
 import com.sep6.backend.models.Account;
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.FetchNotFoundException;
+import org.hibernate.action.internal.EntityActionVetoException;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.util.Optional;
 
 public interface AccountsRepository {
     Optional<Account> findByEmail(String email);
-    Optional<Account> editAccount(int id, Account account);
-    void deleteAccount(int id);
+    Optional<Account> editAccount(int id, Account account) throws AccountNotFoundException, IllegalArgumentException;
+    void deleteAccount(int id) throws IllegalArgumentException;
 
-    Account save(Account user);
-    Account getAccountById(int accountId);
+    Account save(Account user) throws IllegalArgumentException;
+    Optional<Account> getAccountById(int accountId);
 }
--- backend/src/main/java/com/sep6/backend/repository/AccountsRepositoryImpl.java
+++ backend/src/main/java/com/sep6/backend/repository/AccountsRepositoryImpl.java
@@ -1,10 +1,16 @@
 package com.sep6.backend.repository;
 
+import com.sep6.backend.exceptions.EditException;
+import com.sep6.backend.exceptions.SaveException;
 import com.sep6.backend.jpa.AccountsJpaRepository;
 import com.sep6.backend.models.Account;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.hibernate.FetchNotFoundException;
 import org.springframework.stereotype.Repository;
 
+import javax.security.auth.login.AccountNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Repository
@@ -16,40 +22,61 @@ public class AccountsRepositoryImpl implements AccountsRepository{
     @Override
     public Optional<Account> findByEmail(String email)
     {
-        return jpaRepository.findByEmail(email);
+       return jpaRepository.findByEmail(email);
     }
 
-    public Optional<Account> editAccount(int id, Account account)
+    public Optional<Account> editAccount(int id, Account account) throws AccountNotFoundException, IllegalArgumentException
     {
-        Optional<Account> toEdit = jpaRepository.findById(id);
+        try {
+            Optional<Account> toEdit = jpaRepository.findById(id);
 
-        if (toEdit.isPresent()){
-            Account edited = toEdit.get();
-            edited.setEmail(account.getEmail());
-            edited.setName(account.getName());
-            edited.setCountry(account.getCountry());
-            edited.setProfilePictureUrl(account.getProfilePictureUrl());
-            edited.setPassword(account.getPassword());
-            edited.setEnabled(account.isEnabled());
-            return Optional.of(jpaRepository.save(edited));
+            if (toEdit.isPresent()) {
+                Account edited = toEdit.get();
+                edited.setEmail(account.getEmail());
+                edited.setName(account.getName());
+                edited.setCountry(account.getCountry());
+                edited.setProfilePictureUrl(account.getProfilePictureUrl());
+                edited.setPassword(account.getPassword());
+                edited.setEnabled(account.isEnabled());
+                return Optional.of(jpaRepository.save(edited));
+            }
+            return Optional.empty();
+        } catch (NoSuchElementException e) {
+            throw new AccountNotFoundException("Account not found with ID: " + id);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Failed to edit account, because of missing data.", e);
         }
-        return Optional.empty();
     }
 
     @Override
-    public void deleteAccount(int id)
+    public void deleteAccount(int id) throws IllegalArgumentException
     {
-        jpaRepository.disableAccount(id);
+        try
+        {
+            jpaRepository.disableAccount(id);
+        }
+        catch (Exception e)
+        {
+            throw new IllegalArgumentException("Failed to delete account.", e);
+        }
     }
 
     @Override
-    public Account save(Account user)
+    public Account save(Account user) throws IllegalArgumentException
     {
-        return jpaRepository.save(user);
+        try
+        {
+            return jpaRepository.save(user);
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw new IllegalArgumentException("Failed to save account.", e);
+        }
     }
 
     @Override
-    public Account getAccountById(int accountId) {
-        return jpaRepository.findById(accountId).get();
+    public Optional<Account> getAccountById(int accountId)
+    {
+        return jpaRepository.findById(accountId);
     }
 }
