5f1b3bfda977e9349558559a3567d75e0b1e484a
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-19 08:00:14 +0200

    SEP6-36: add missing import and remove not used imports

--- backend/src/main/java/com/sep6/backend/service/AuthenticationService.java
+++ backend/src/main/java/com/sep6/backend/service/AuthenticationService.java
@@ -1,12 +1,13 @@
 package com.sep6.backend.service;
 
-import com.sep6.backend.models.Account;
 import com.sep6.backend.models.auth.AuthenticationRequest;
 import com.sep6.backend.models.auth.AuthenticationResponse;
 import com.sep6.backend.models.auth.RegisterRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
+
+import java.io.IOException;
 
 public interface AuthenticationService {
     AuthenticationResponse register(RegisterRequest request) throws IllegalArgumentException;
