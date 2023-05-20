4bf3dea8ced32dda53f772bef377d9c0cfb36fee
Georgiana <ion.georgiana@hotmail.com>
2023-05-20 16:28:56 +0300

    casing

---
+++ frontend/src/pages/home-page.tsx
@@ -0,0 +1,110 @@
+import { Button, FormControl, Grid, InputLabel, MenuItem, Pagination, Select, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from 'tss-react/mui';
+import CarouselComponent from '../components/Carousel';
+import MovieCard from '../components/MovieCard';
+import { Colors } from '../constants/Colors';
+import { SelectChangeEvent } from '@mui/material';
+import MovieService from "../services/movies";
+import GenreFilter from '../components/GenreFilter';
+import { Movie } from "../types";
+
+const HomePage = () => {
+  const { classes } = useStyles();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<number | "">("");
+
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const movies = await MovieService.filterMovies(currentPage, selectedGenre);
+        setMovies(movies);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, [currentPage, selectedGenre]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handleGenreChange = (event: SelectChangeEvent<number>) => {
+    const selectedGenreValue = event.target.value;
+    setSelectedGenre(selectedGenreValue as number);
+  };
+  
+  
+  
+
+  return (
+    <Grid container>
+
+      <Grid item lg={6} display='grid' justifyContent="flex-start">
+        <Typography variant="h1" className={classes.heroText}>DISCOVER THE UNIVERSE OF MOVIES</Typography>
+        <Button className={classes.button} variant="contained">
+          Get Started
+        </Button>
+      </Grid>
+      <Grid item lg={6}>
+        <CarouselComponent movies={movies} />
+      </Grid>
+
+      <Grid item container>
+        <Grid item lg={12} className={classes.genreContainer}>
+          <GenreFilter selectedGenre={selectedGenre} onChange={handleGenreChange} />
+          </Grid>
+
+        {
+           movies.map((movie: Movie, index: number) => (
+            <Grid key={index} item lg={3}>
+              <MovieCard poster={movie.posterUrl} title={movie.title} id={movie.id}/>
+            </Grid>
+          ))
+        }
+
+      {selectedGenre === "" &&
+      <Grid item className={classes.paginationContainer} lg={12}>
+        <Pagination
+          className={classes.pagination}
+          count={21}
+          page={currentPage}
+          onChange={(event, page) => handlePageChange(page)}
+        />
+        </Grid>}
+      </Grid>
+    </Grid>
+  );
+};
+
+const useStyles = makeStyles()(() => ({
+  heroText: {
+    lineHeight: '8.5rem',
+    textAlign: 'start',
+    maxWidth: '35rem',
+  },
+  button:{
+    width: '23.625rem',
+  },
+  paginationContainer:{
+    display:'grid',
+    justifyContent: 'center',
+  },
+  pagination:{
+    "& .MuiPaginationItem-root": {
+      color: Colors.lightCyan
+    },
+  },
+  genreContainer:{
+    display:'grid',
+    justifyContent: 'end',
+    padding: '1rem',
+  },
+}));
+
+
+export default HomePage;
---
+++ frontend/src/services/authentication.tsx
@@ -0,0 +1,63 @@
+import axios from '../api/axios';
+
+const API_URL = '/auth';
+
+
+
+const signup = (name:string, email:string, username:string, password:string, country:string, gender:string, dateOfBirth:string) => {
+
+  const profilePictureUrl=""
+  const role="USER"
+
+  return axios
+    .post(API_URL + "/register", {
+      name,
+      email,
+      username,
+      password,
+      country,
+      profilePictureUrl,
+      dateOfBirth,
+      gender,
+      role
+    })
+    .then((response: { data: { accessToken: any; }; }) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const login = (email: string, password: string) => {
+  return axios
+    .post(API_URL + "/authenticate", {
+      email,
+      password,
+    })
+    .then((response: { data: { accessToken: any; }; }) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user")|| '{}');
+};
+
+const authService = {
+  signup,
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default authService;
