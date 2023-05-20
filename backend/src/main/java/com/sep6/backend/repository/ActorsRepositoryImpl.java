044fa9d84f57126209c93d212b4041689c3dd659
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-20 09:06:32 +0200

    SEP6-29: add logging to repository classes

--- backend/src/main/java/com/sep6/backend/repository/ActorsRepositoryImpl.java
+++ backend/src/main/java/com/sep6/backend/repository/ActorsRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.sep6.backend.models.PersonType;
 import com.sep6.backend.projections.PersonMoviesProjection;
 import com.sep6.backend.projections.PersonProjection;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,34 +14,38 @@ import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
+@Slf4j
 public class ActorsRepositoryImpl implements ActorsRepository
 {
     private PeopleJpaRepository jpaRepository;
 
     @Override
-    public List<PersonProjection> getActors()
-    {
+    public List<PersonProjection> getActors() {
+        log.info("Getting all actors");
         return jpaRepository.getAllByType(PersonType.ACTOR);
     }
 
     @Override
     public List<PersonProjection> getActorsBySearch(String search) {
+        log.info("Getting actors by search: {}", search);
         return jpaRepository.findAllByTypeAndNameContainingIgnoreCase(PersonType.ACTOR, search);
     }
 
     @Override
     public Optional<PersonMoviesProjection> getActorById(int id) {
+        log.info("Getting actor by ID: {}", id);
         return jpaRepository.findByTypeAndId(PersonType.ACTOR, id);
     }
 
     @Override
     public Optional<Person> findById(int id) {
+        log.info("Finding person by ID: {}", id);
         return jpaRepository.findById(id);
     }
 
     @Override
-    public Person save(Person person)
-    {
+    public Person save(Person person) {
+        log.info("Saving person: {}", person);
         return jpaRepository.save(person);
     }
 }
--- backend/src/main/java/com/sep6/backend/repository/ReviewsRepositoryImpl.java
+++ backend/src/main/java/com/sep6/backend/repository/ReviewsRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.sep6.backend.models.Account;
 import com.sep6.backend.models.Movie;
 import com.sep6.backend.models.Review;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
+@Slf4j
 public class ReviewsRepositoryImpl implements ReviewsRepository {
     private ReviewsJpaRepository jpaRepository;
     private MoviesRepository moviesRepository;
@@ -21,12 +23,14 @@ public class ReviewsRepositoryImpl implements ReviewsRepository {
 
     @Override
     public List<Review> getMovieReviews(int id) {
+        log.info("Getting reviews for movie with ID: {}", id);
         return jpaRepository.findReviewsByMovieId(id);
     }
 
     @Override
-    public Review createMovieReview(Review review)
-    {
+    public Review createMovieReview(Review review) {
+        log.info("Creating movie review: {}", review);
+
         if (review.getCreatedOn() == null)
             review.setCreatedOn(LocalDateTime.now());
         Movie movieById = moviesRepository.getMovieById(review.getMovieId()).orElseThrow();
@@ -39,8 +43,9 @@ public class ReviewsRepositoryImpl implements ReviewsRepository {
     }
 
     @Override
-    public Review updateMovieReview(Review review)
-    {
+    public Review updateMovieReview(Review review) {
+        log.info("Updating movie review: {}", review);
+
         Review reviewById = getReviewById(review.getId()).orElseThrow();
         reviewById.setComment(review.getComment());
         reviewById.setRating(review.getRating());
@@ -49,13 +54,13 @@ public class ReviewsRepositoryImpl implements ReviewsRepository {
     }
 
     @Override
-    public Review deleteReview(int reviewId)
-    {
+    public Review deleteReview(int reviewId) {
+        log.info("Deleting review with ID: {}", reviewId);
         return jpaRepository.deleteById(reviewId);
     }
 
-    public Optional<Review> getReviewById(int id)
-    {
+    public Optional<Review> getReviewById(int id) {
+        log.info("Getting review by ID: {}", id);
         return jpaRepository.findById(id);
     }
 }
