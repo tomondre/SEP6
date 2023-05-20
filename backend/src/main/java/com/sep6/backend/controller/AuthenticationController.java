08ca349990a9a6cc6cc8abe7d3fcee8a2263a233
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-20 09:05:46 +0200

    SEP6-29: add logging to controller classes

--- backend/src/main/java/com/sep6/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/sep6/backend/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import com.sep6.backend.service.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
@@ -22,64 +23,54 @@ import java.util.NoSuchElementException;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticationController
 {
 
     private final AuthenticationService service;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request)
-    {
-        try
-        {
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        try {
+            log.info("Registering user: {}", request.getUsername());
             return ResponseEntity.ok(service.register(request));
-        }
-        catch (IllegalArgumentException e)
-        {
+        } catch (IllegalArgumentException e) {
+            log.error("Invalid registration request", e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
-        catch (Exception e)
-        {
-            //TODO log the error
+        } catch (Exception e) {
+            log.error("An error occurred during registration", e);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong, please try again later");
         }
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request)
-    {
-        try
-        {
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        try {
+            //TODO test if email exists
+            log.info("Authenticating user: {}", request.getEmail());
             return ResponseEntity.ok(service.authenticate(request));
-        }
-        catch (IllegalArgumentException e)
-        {
+        } catch (IllegalArgumentException e) {
+            log.error("Invalid authentication request", e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
-        catch (AuthenticationException e)
-        {
+        } catch (AuthenticationException e) {
+            log.warn("Authentication failed for user: {}", request.getEmail(), e);
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
-        }
-        catch (Exception e)
-        {
-            //TODO log the error
+        } catch (Exception e) {
+            log.error("An error occurred during authentication", e);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong, please try again later");
         }
     }
 
     @PostMapping("/refresh-token")
-    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException
-    {
-        try
-        {
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            log.info("Refreshing token");
             service.refreshToken(request, response);
-        }
-        catch (NoSuchElementException e)
-        {
+        } catch (NoSuchElementException e) {
+            log.warn("Invalid refresh token", e);
             response.sendError(HttpStatus.NOT_FOUND.value(), "The refresh token is not valid");
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
+            log.error("An error occurred while refreshing token", e);
             response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Something went wrong, please try again later");
         }
     }
