94d24ea2709ec1905c597dcaa5ba703b145bfc9a
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-21 11:41:22 +0200

    SEP6-29: add logging to repository classes

--- backend/src/main/java/com/sep6/backend/repository/PeopleRepositoryImpl.java
+++ backend/src/main/java/com/sep6/backend/repository/PeopleRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.sep6.backend.jpa.PeopleJpaRepository;
 import com.sep6.backend.projections.PersonMoviesProjection;
 import com.sep6.backend.projections.PersonProjection;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
@@ -13,26 +14,31 @@ import java.util.Optional;
 
 @AllArgsConstructor
 @Repository
+@Slf4j
 public class PeopleRepositoryImpl implements PeopleRepository{
     private PeopleJpaRepository repository;
 
     @Override
     public List<PersonProjection> getPeopleBySearch(String search) {
+        log.info("Fetching people by search: {}", search);
         return repository.findAllByNameContainingIgnoreCase(search);
     }
 
     @Override
     public List<PersonProjection> getPeople() {
+        log.info("Fetching all people");
         return repository.findAllByIdNotNull();
     }
 
     @Override
     public Optional<PersonMoviesProjection> getPersonById(int id) {
+        log.info("Fetching person by id: {}", id);
         return Optional.of(repository.findFirstById(id));
     }
 
     @Override
     public List<PersonProjection> getPaginatedPeople(int page) {
+        log.info("Fetching paginated people: page {}", page);
         Pageable pageable = PageRequest.of(page, 12);
         return repository.findAll(pageable).getContent();
     }
