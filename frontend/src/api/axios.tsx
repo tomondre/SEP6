89ee12b3ab21f637c6d386c5377b7196b19a75bf
tomondre <tomas.ondrejka2000@gmail.com>
2023-05-25 14:09:56 +0700

    Changing the base url to point to the cloud api

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://sep6-api.tomondre.com",
   headers: {
     "Content-type": "application/json",
   },
