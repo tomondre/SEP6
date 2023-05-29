6f329e1f32fca5558a7460b20e8cea784a81eaa4
Khaled Hammoun <kh.hammoun@gmail.com>
2023-05-29 11:38:16 +0200

    SEP6-85: add logging to the cronjob

--- backend/src/main/java/com/sep6/backend/util/TokenCleanupService.java
+++ backend/src/main/java/com/sep6/backend/util/TokenCleanupService.java
@@ -1,10 +1,9 @@
 package com.sep6.backend.util;
 
-import com.sep6.backend.jpa.TokenJpaRepository;
 import com.sep6.backend.repository.TokenRepository;
 import com.sep6.backend.security.token.Token;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,13 +11,14 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class TokenCleanupService
 {
     private final TokenRepository tokenRepository;
 
     @Scheduled(cron = "0 0 0 * * *") // Run at midnight every day
     public void cleanExpiredTokens() {
-        // Implement the logic to query and delete expired records
+        log.info("Cleaning expired tokens");
         List<Token> expiredTokens = tokenRepository.getAllExpiredTokens();
         tokenRepository.deleteAll(expiredTokens);
     }
